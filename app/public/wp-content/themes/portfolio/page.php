8db6957e3380e4b513990fb200dceac71f9c7694
luismartinezs <martinez.s.luis@gmail.com>
2019-10-13 22:47:03 +0200

    Policy page content is loaded from WordPress

--- app/public/wp-content/themes/portfolio/page.php
+++ app/public/wp-content/themes/portfolio/page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file
  *
@@ -19,32 +20,25 @@ get_header();
 
 <main>
     <section name="privacy" class="pt-50 privacy-section">
-      <a id="privacy" class="section__anchor">&nbsp;</a>
-      <div class="container">
-        <h2 class="section__title privacy__title font-weight-normal">Privacy Policy</h2>
-        <p>Your privacy is important to us. It is our policy to respect your privacy regarding any
-          information we may collect from you across our website, and other sites we own and operate.</p>
-        <p>We only ask for personal information when we truly need it to provide a service to you. We collect it by fair
-          and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it
-          will be used.</p>
-        <p>We only retain collected information for as long as necessary to provide you with your requested service.
-          What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as
-          unauthorised access, disclosure, copying, use or modification.</p>
-        <p>We don’t share any personally identifying information publicly or with third-parties, except when required to
-          by law.</p>
-        <p>Our website may link to external sites that are not operated by us. Please be aware that we have no control
-          over the content and practices of these sites, and cannot accept responsibility or liability for their
-          respective privacy policies.</p>
-        <p>You are free to refuse our request for your personal information, with the understanding that we may be
-          unable to provide you with some of your desired services.</p>
-        <p>Your continued use of our website will be regarded as acceptance of our practices around privacy and personal
-          information. If you have any questions about how we handle user data and personal information, feel free to
-          contact us.</p>
-        <p>This policy is effective as of 24 September 2019.</p>
+        <a id="privacy" class="section__anchor">&nbsp;</a>
+        <div class="container">
+            <?php
 
-      </div>
+            /* Start the Loop */
+            while (have_posts()) :
+                the_post(); ?>
+
+                <h2 class="section__title privacy__title font-weight-normal"><?php the_title(); ?></h2>
+
+                <?php the_content(); ?>
+
+            <?php
+            endwhile; // End of the loop.
+            ?>
+
+        </div>
     </section>
-  </main>
+</main>
 
 
 <?php
